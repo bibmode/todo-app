252e5813cfba066bb1faab4c77b73d4ca6697180
Genevieve <vievenavales29@gmail.com>
2021-10-02 14:27:19 +0800

    packages and starting customizations

---
+++ src/AppDivider/AppDivider.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 60.8rem;
+
+  h1 {
+    font-size: 3.6rem;
+    font-weight: 700;
+  }
+`;
---
+++ src/AppDivider/index.js
@@ -0,0 +1,11 @@
+import { Wrapper } from "./AppDivider.styles";
+
+const AppDivider = () => {
+  return (
+    <Wrapper>
+      <h1>#todo</h1>
+    </Wrapper>
+  );
+};
+
+export default AppDivider;
